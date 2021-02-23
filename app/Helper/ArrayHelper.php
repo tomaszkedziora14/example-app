36af602892773e6126e1f088891d55f8e9d2993e
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2021-02-23 01:32:08 +0100

    fix

--- app/Helper/ArrayHelper.php
+++ app/Helper/ArrayHelper.php
@@ -6,38 +6,14 @@ use Excel;
 
 class ArrayHelper
 {
-    private $headers;
-
     private $combinedRows = [];
-
-    private $repeatableSameKey = [];
-
-    private $items = [];
 
     public function excelToArray($filePath)
     {
-
-      // $data[] = [
-      //     'title' => 'DPO_Filter:Anlass',
-      //     'libs' => [
-      //         [
-      //             'name' => 'Beitragsänderung'
-      //         ],
-      //         [
-      //             'name' => 'Beitragsänderung'
-      //         ]
-      //     ]
-      // ];
-
-
-
         $arrayXlsx = Excel::toArray([], $filePath);
-
         $headers = $arrayXlsx[0][0];
         $headers = preg_grep ('/^DPO_Filter/', $headers);
-
         unset($arrayXlsx[0][0]);
-
         $xlsxArr = $arrayXlsx[0];
 
         array_splice($xlsxArr, 0, 1);
@@ -46,19 +22,7 @@ class ArrayHelper
         for($i=0; $i<$count; $i++){
             $this->mergeRowsAndAddNewKeys($xlsxArr, $i, $headers);
         }
-
-      //  dd($this->combinedRows);
-
          return $this->combinedRows;
-    }
-
-    private function merge($data, $k)
-    {
-        $array = [];
-        foreach($data as $key=>$arr){
-            $array[] = $data[$key][$k];
-        }
-        $this->combinedRows[] = $array;
     }
 
     private function mergeRowsAndAddNewKeys($data, $k, $headers)
@@ -68,20 +32,6 @@ class ArrayHelper
           $array['title'] = $headers[$k];
           $array['libs'][]['name'] = $data[$key][$k];
         }
-
-
-
-      //  dd($array);
         $this->combinedRows[] = $array;
-    }
-
-    public function createRepitableKey()
-    {
-      $array = [];
-      $keyName = 'name';
-      for($i=0; $i<5; $i++){
-        $array[] = $keyName.$i;
-      }
-      return  $array;
     }
 }
